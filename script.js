b1ef090fec4d453c490c6b15df56a85646918ca1
NJEI03 <njeipierrick@gmail>
2024-09-10 20:48:19 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+/* // Set the date for tomorrow at 00:00:00 */
+const countdownDate = new Date("Sep 11, 2024 00:00:00").getTime();
+
+/* // Update the countdown every second */
+const x = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    /* //Calculate time left in days, hours, minutes, and seconds */
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    /* //Display the result */
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    /* // If the countdown is finished, display the birthday message */
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").style.display = "none";
+        document.getElementById("title").style.display = "none";
+        document.getElementById("birthday-message").style.display = "block";
+    }
+}, 1000);
